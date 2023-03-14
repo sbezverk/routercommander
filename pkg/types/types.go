1dcf9b44c4ce821e0298531b2d59a6c9e09efddc
Serguei Bezverkhi <sbezverk@cisco.com>
2023-03-14 19:13:46 +0100

    initial bits for value capturing

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -3,15 +3,16 @@ package types
 import "regexp"
 
 type Command struct {
-	Cmd           string   `yaml:"command"`
-	Times         int      `yaml:"times"`
-	Interval      int      `yaml:"interval"`
-	WaitBefore    int      `yaml:"wait_before"`
-	WaitAfter     int      `yaml:"wait_after"`
-	Location      []string `yaml:"location"`
-	Pattern       []string `yaml:"pattern"`
-	Debug         bool     `yaml:"debug"`
-	CollectResult bool     `yaml:"collect_result"`
+	Cmd           string     `yaml:"command"`
+	Times         int        `yaml:"times"`
+	Interval      int        `yaml:"interval"`
+	WaitBefore    int        `yaml:"wait_before"`
+	WaitAfter     int        `yaml:"wait_after"`
+	Location      []string   `yaml:"location"`
+	Pattern       []string   `yaml:"pattern"`
+	Debug         bool       `yaml:"debug"`
+	CollectResult bool       `yaml:"collect_result"`
+	Patterns      []*Pattern `yaml:"patterns"`
 	RegExp        []*regexp.Regexp
 }
 
@@ -27,4 +28,16 @@ type Commander struct {
 	List    []*Command `yaml:"commands"`
 	Repro   *Repro     `yaml:"repro"`
 	Collect *Collect   `yaml:"collect"`
+}
+
+type Pattern struct {
+	PatternString string   `yaml:"pattern_string"`
+	Capture       *Capture `yaml:"capture"`
+	RegExp        *regexp.Regexp
+}
+
+type Capture struct {
+	FieldNumber int    `yaml:"field_number"`
+	Separator   string `yaml:"separator"`
+	Value       interface{}
 }
